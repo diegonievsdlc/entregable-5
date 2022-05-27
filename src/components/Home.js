8897b49cdb22fdb6e896acc0669c3022c2a54bf7
diegonievsdlc <algorapidonose@gmail.com>
2022-05-27 12:49:46 -0500

    ADD: project

---
+++ src/components/Home.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import logo from "../assets/image 11.png";
+import ornament from "../assets/Group 216 (1).png";
+import { useDispatch } from "react-redux";
+import { getName } from "../store/slices/name.slice";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const submit = (e) => {
+    e.preventDefault();
+    dispatch(getName(name));
+    navigate('/pokedex')
+  };
+  return (
+    <div className="Home">
+      <div>
+        <img src={logo} alt="Logo" />
+        <h1>¡Hola entrenador!</h1>
+        <h2>Para poder comenzar, dame tu nombre</h2>
+        <form onSubmit={submit}>
+          <input
+            placeholder="Tu nombre..."
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <button>Comenzar</button>
+        </form>
+      </div>
+      <img className="ornament-home" src={ornament} alt="Ornament" />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Pokedex.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import ornament from '../assets/Group 216 (2).png'
+import logo from '../assets/image 11.png'
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+
+const Pokedex = () => {
+  const name = useSelector(state => state.name)
+  const [types, setTypes] = useState([])
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/type')
+      .then(res => setTypes(res.data.results))
+  }, [])
+  return (
+    <div className='Pokedex'>
+      <img className='ornament-pokedex' src={ornament} alt="Ornament" />
+      <img className='logo-pokedex' src={logo} alt="Logo" />
+      <h2><span>Bienvenido {name}</span>, aqui podras encontar tu pokemon preferido</h2>
+      <div className="search">
+        <div className="search-for-name">
+          <input type="text" />
+          <button>Buscar</button>
+        </div>
+        <select className='search-for-type'>
+          <option value="all-pokemons">Todos los pokemones</option>
+          {
+            types.map(type => (
+              <option key={type.name}>
+                {type.name}
+              </option>
+            ))
+          }
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Pokedex;
